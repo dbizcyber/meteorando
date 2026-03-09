97a3a024f8764b1d82d7a4a2f8d43b27a45bdf37
dbizcyber <dbizard@gmail.com>
2026-03-09 12:29:46 +0100

    Add app.js for managing event listeners and UI updates

---
+++ js/app.js
@@ -0,0 +1,114 @@
+import { remplirMenu } from "./menuRandos.js";
+import { activerRecherche } from "./rechercheRandos.js";
+import { initHoraires } from "./horairesRando.js"
+import { remplirMenuAnimateurs } from "./menuAnimateurs.js";
+import { activerRechercheAnimateur } from "./rechercheAnimateur.js";
+import { remplirMenuParkings } from "./menuParkings.js";
+import { initCarte, chercherLieu } from "./carteParking.js"
+import { calculCovoiturage } from "./covoiturage.js"
+import { initGPX } from "./gpxAnalyse.js"
+import { initProfilGPX } from "./profilAltitude.js"
+import { afficherMeteo } from "./meteoRando.js"
+import { initResume } from "./resumeRando.js"
+import { initEnvoi } from "./envoiRando.js"
+
+function gestionAutreAnimateur(){
+
+const select = document.getElementById("animateur");
+const champ = document.getElementById("nouvelAnimateur");
+
+select.addEventListener("change", () => {
+
+if(select.value.startsWith("Autre")){
+
+champ.style.display = "block";
+champ.focus();
+
+}else{
+
+champ.style.display = "none";
+champ.value = "";
+
+}
+
+});
+
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+/* randos */
+
+remplirMenu();
+activerRecherche();
+
+initHoraires()
+/* animateurs */
+
+remplirMenuAnimateurs();
+activerRechercheAnimateur();
+gestionAutreAnimateur();
+
+/* park_covoiturage*/
+remplirMenuParkings();
+gestionAutreParking();
+
+/* park_départ_randos*/
+initCarte()
+
+document
+.getElementById("btnGeocoder")
+.addEventListener("click", chercherLieu)
+
+/* cout_voiturage*/
+document
+.getElementById("autoroute")
+.addEventListener("input", calculCovoiturage)
+
+initProfilGPX()
+
+});
+
+/* analyse gpx*/
+document.addEventListener("DOMContentLoaded", () => {
+
+remplirMenu()
+activerRecherche()
+
+remplirMenuAnimateurs()
+activerRechercheAnimateur()
+
+gestionAutreAnimateur()
+
+initCarte()
+
+initGPX()   // ← ajout ici
+
+initResume()
+
+initEnvoi()
+
+})
+function gestionAutreParking(){
+
+const select = document.getElementById("parking");
+const champ = document.getElementById("nouveauParking");
+
+select.addEventListener("change", () => {
+
+if(select.value.startsWith("Autre")){
+
+champ.style.display = "block";
+champ.focus();
+
+}else{
+
+champ.style.display = "none";
+champ.value = "";
+
+}
+
+});
+
+}
