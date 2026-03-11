9b0d138b89265553fec609a904c43e96d4b5dac4
dbizcyber <dbizard@gmail.com>
2026-03-11 07:40:57 +0100

    Update envoiRando.js

--- js/envoiRando.js
+++ js/envoiRando.js
@@ -15,7 +15,7 @@ const emailUser =
 document.getElementById("emailUser").value
 
 const response = await fetch(
-"https://https://whlxbfnmyqdflmxosfse.supabase.co/functions/v1/envoi-rando",
+"https://https://whlxbfnmyqdflmxosfse.supabase.co/functions/v1/dynamic-handler",
 {
 method: "POST",
 headers:{
